fc949afb8df75b023914ee6e55628d25df44d790
Chandeep Singh <chandeep.singh@flipp.com>
2018-05-30 17:52:37 -0400

    add panzoom

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var draw = SVG('drawing').size(750, 400)
-draw.rect(750, 750).fill('#EBEDEF')
+var width = 750
+var height = 500
+var draw = SVG('drawing')
+  .size(width, height)
+  .panZoom({zoomMin: 0.5, zoomMax: 20})
 
 c = new Controller(draw)
+var b = new Box(200, 100, draw, c)
 
 document.querySelector("#add_box").onclick = function() {
-  new Box(100, 50, draw, c)
+  new Box(200, 100, draw, c)
 }
 
 document.querySelector("#add_line").onclick = function() {
   c.create_line = true
+}
+
+document.querySelector("#reset_zoom").onclick = function() {
+  draw.zoom(1)
 }
